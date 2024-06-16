5a35bd4a17932ea1cc6218c0ca8893ca2eb7d8fb
ELCRND <jwhh2a@gmail.com>
2024-06-16 20:28:13 +0300

    modified:   index.html 	new file:   public/img/contacts/contacts_bg.png 	modified:   style.css 	modified:   swiper.js

--- swiper.js
+++ swiper.js
@@ -2,11 +2,28 @@ import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
 
 const brandsSwiper = new Swiper(".swiper-brands", {
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1.5,
   spaceBetween: 30,
   navigation: {
     nextEl: ".swiper-brands-button-next",
     prevEl: ".swiper-brands-button-prev",
+  },
+  breakpoints: {
+    400: {
+      slidesPerView: 2,
+    },
+    600: {
+      slidesPerView: 3,
+    },
+    980: {
+      slidesPerView: 4,
+    },
+    1280: {
+      slidesPerView: 5,
+    },
+    1440: {
+      slidesPerView: 6,
+    },
   },
 });
 
@@ -20,19 +37,54 @@ const answersSwiper = new Swiper(".swiper-answers", {
 });
 
 const promotionsSwiper = new Swiper(".swiper-promotions", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
   navigation: {
     nextEl: ".swiper-promotions-button-next",
     prevEl: ".swiper-promotions-button-prev",
   },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
 });
 
 const instructionsSwiper = new Swiper(".swiper-instructions", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
   navigation: {
     nextEl: ".swiper-instructions-button-next",
     prevEl: ".swiper-instructions-button-prev",
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const reviewsSwiper = new Swiper(".swiper-reviews", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  freeMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-reviews-button-next",
+    prevEl: ".swiper-reviews-button-prev",
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 4,
+    },
   },
 });
